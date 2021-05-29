2c454274e57de84ac9ec5d0da116dee3ae44f14a
Vishal Vijay <0vishalvijay0@gmail.com>
2021-05-30 00:25:49 +0530

    Fix build 9

--- next.config.js
+++ next.config.js
@@ -1,12 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // next.config.js
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+const nextTranslate = require('next-translate');
 const nextPwa = require('next-pwa');
 
 const nextConfig = {
-  target: "experimental-serverless-trace",
+  target: 'experimental-serverless-trace',
 };
 
-const plugins = [nextPwa];
+const plugins = [
+  optimizedImages,
+  nextTranslate,
+  [
+    nextPwa,
+    {
+      pwa: {
+        dest: 'public',
+        disable: process.env.NODE_ENV === 'development',
+        sw: 'service-worker.js',
+        importScripts: [
+          'https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js',
+        ],
+      },
+    },
+  ],
+];
 
 module.exports = withPlugins(plugins, nextConfig);
